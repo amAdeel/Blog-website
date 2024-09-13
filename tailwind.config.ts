9ea6683e93839b7b146d394ba664138e9ccac5c6
amAdeel <adeeleducationpurpose@gmail.com>
2024-09-13 19:43:58 +0500

    updating 01

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -16,4 +17,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
